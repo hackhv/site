6f7fc392f7ea4d97c69bc45c62031023c8c02fb6
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-04-15 20:32:02 -0400

    rewrite nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,22 @@
 import React from 'react'
-import { Box, Flex, Link as A } from '@hackclub/design-system'
+import { Box, Container, Flex, Link as A } from '@hackclub/design-system'
 import Flag from 'components/Flag'
 import Link from 'gatsby-link'
 
-const Base = Flex.extend.attrs({
-  pt: 3,
-  px: [3, 4],
-  pb: 2,
-  justify: 'space-around',
-  w: 1
-})`
+const Base = Box.extend.attrs({ py: 3, w: 1 })`
   position: absolute;
   top: 0;
-  left: 50%;
-  transform: translateX(-50%);
   z-index: 4;
-  align-items: center;
 `
 
-const NavBar = Flex.withComponent('nav').extend`
-  overflow-x: auto;
-  -webkit-overflow-scrolling: touch;
+const Inner = Container.withComponent('nav').extend.attrs({
+  maxWidth: 48,
+  px: 3,
+  role: 'naviation'
+})`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
 
 export const Item = A.withComponent(Link).extend.attrs({
@@ -31,11 +27,13 @@ export const Item = A.withComponent(Link).extend.attrs({
 
 const Nav = ({ color = 'white', ...props }) => (
   <Base role="banner" {...props}>
-    <Flag />
-    <NavBar role="navigation" ml={3} py={[1, 0]} color={color} align="center">
-      <Item to="/register" children="Register" />
-      <Item to="/about" children="About" />
-    </NavBar>
+    <Inner color={color} py={[1, 0]}>
+      <Flag />
+      <Box ml={3}>
+        <Item to="/register" children="Register" />
+        <Item to="/about" children="About" />
+      </Box>
+    </Inner>
   </Base>
 )
 
