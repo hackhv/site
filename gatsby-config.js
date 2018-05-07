9237eeca388260c0c83a8885799a1fa5950ababd
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-05-07 00:09:41 -0400

    Add favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,24 @@ module.exports = {
       }
     },
     {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './static/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+    {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-45778683-3',
