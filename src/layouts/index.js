1444cbaeafd3e2d680a22a3470bae433ef359f95
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-05-06 23:41:25 -0400

    Add Heap

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,6 +10,10 @@ const meta = tags =>
   tags.map((props, index) =>
     React.createElement('meta', { ...props, key: index })
   )
+const heap = `
+  window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+  heap.load("3545039032");
+`
 
 export default props => (
   <ThemeProvider>
@@ -40,6 +44,7 @@ export default props => (
       <link rel="stylesheet" href="/fonts.css" />
       <script type="application/ld+json" children={JSON.stringify(org)} />
       <script type="application/ld+json" children={JSON.stringify(event)} />
+      <script dangerouslySetInnerHTML={{ __html: heap }} />
     </Helmet>
     {props.children()}
   </ThemeProvider>
