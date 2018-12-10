88ef0c2aafe2630a5485e293dbd2f0a854a54e1c
Zane Davis-Barrs <github@zane.sh>
2018-12-09 16:47:16 -0800

    Fix grammatical error

--- src/pages/index.js
+++ src/pages/index.js
@@ -144,7 +144,7 @@ export default () => (
         <Container maxWidth={48}>
           <Heading.h2 f={[3, 4]} mb={4}>
             <Text.span bg="warm" px={1}>New!</Text.span> Hack Happy Valley is now
-            Hack Pennsylvania a 200-person event in State College in January.
+            Hack Pennsylvania—a 200-person event in State College in January.
             Below is our old site.
           </Heading.h2>
         </Container>
